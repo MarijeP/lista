9b1bb8d0647ea53aa1843d0c2ec04e6b0bb4c0f6
Marije Pierson <marijepierson@gmail.com>
2026-02-21 11:51:04 +0200

    Add config file and tweaks

---
+++ config.js
@@ -0,0 +1,14 @@
+// ================================================================
+// YOUR FIREBASE CONFIG — fill this in once and never touch it again
+// ================================================================
+export const firebaseConfig = {
+  apiKey: "AIzaSyCP8qyXDwXKjFcTvOrH5Y-PL9JoC-lzLi0",
+  authDomain: "shlist-7466f.firebaseapp.com",
+  databaseURL: "https://shlist-7466f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "shlist-7466f",
+  storageBucket: "shlist-7466f.firebasestorage.app",
+  messagingSenderId: "678202976422",
+  appId: "1:678202976422:web:6ff000ebcd0909a217ec67",
+  measurementId: "G-EFWXV5029Q"
+};
+// ================================================================
