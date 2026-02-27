2810fcd3cd127fd50c7fe2e60aca3c1fbd1d749f
Marije Pierson <marijepierson@gmail.com>
2026-02-27 15:16:22 +0200

    Making it PWA

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE = 'lista-v1';
+const ASSETS = [
+  '/lista/',
+  '/lista/index.html',
+  '/lista/manifest.json',
+  '/lista/icon-192.png',
+  '/lista/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Only cache GET requests for our own assets
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const network = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(cache => cache.put(e.request, clone));
+        }
+        return res;
+      });
+      return cached || network;
+    })
+  );
+});
